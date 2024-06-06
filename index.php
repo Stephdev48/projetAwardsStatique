2bc1d8d0eb278941f48e1416238c2dfa559eaeda
noedel <noe.dwwm48@gmail.com>
2024-06-06 11:24:50 +0200

    section inscription
    presque terminado (il manque deux 3 détails css)

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<<<<<<< Updated upstream:index.php
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <title>Document</title>
+=======
+    <link href="style/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- Version 1.10: Icons Bootstrap included in HTML head -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <title>DW/WM AWARDS 2024</title>
+>>>>>>> Stashed changes:index.html
 </head>
 <body>
     <header class="main_background">
@@ -36,22 +46,25 @@
     <section class="section_1">
         <div class="container">
             <div class="row">
-                <div id="affiche" class="col-6 affiche">
+                <div id="affiche" class="col-5 affiche">
                         <span class="fs-1 fw-bold text-dark">DWWM 48 AWARDS</span>
                         <img src="images/coupe.png" alt="coupe_vainqueur" width="150px" class="my-4"/>
                         <div class="d-flex flex-row justify-content-between">
                             <p class="fw-bold">Mercredi 12 juin 2024</p>
-                            <p class="fw-bold">Adresse Agustin</p>
+                            <p class="fw-bold">6 impasse du Général Éclair</p>
                         </div>
                 </div>
-                <div class="col-6 inscription">
+                <div class="col-5 inscription">
                     <h3>Mercredi 12 juin 2024</h3>
-                    <span>Date d'échéance des inscriptions : 5 juin</span>
-                    <span>Chez Agustin !</span>
-                    <span>10€, amener à boire ou à manger</span>
-                    <form>
-                        <input type="text" name="nom_inscrit">
-                        <button id="bouton_inscription">S'inscrire</button>
+                    <span>Date d'échéance des inscriptions : 5 juin</span><br>
+                    <i class="bi bi-geo-alt-fill"></i>
+                    <a href="https://fr.mappy.com/plan#/6%20Impasse%20du%20G%C3%A9n%C3%A9ral%20Leclerc%2C%2048000%20Mende"><span class="adresse">6 impasse du Général Leclerc</span></a><br><hr>
+                    <i class="bi bi-ticket-fill"></i>
+                    <span>10€ <br> (Amener à boire ou à manger)</span><br><hr>
+                    <form class="formulaire_inscription">
+                        <input placeholder="Nom Prénom" type="text" name="nom_inscrit" class="nomprenom"><br>
+                        <p>10 places restantes.</p>
+                        <button id="bouton_inscription" class="btn_inscription">S'inscrire</button>
                     </form>
                 </div>
             </div>
